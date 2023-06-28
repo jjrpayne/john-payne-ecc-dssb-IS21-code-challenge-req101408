2c6c01ec97c09bc6443175d3185499f9d835ac5d
John Payne <jjrpayne@hotmail.com>
2023-06-27 23:06:27 -0700

    added swagger documentation

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+      openapi: "3.1.0",
+      info: {
+        title: "ECC Product API",
+        version: "1.0.0",
+        description:
+          "This is an API that keeps track of products for the BC Ministry of Education and Child Care",
+        contact: {
+          name: "John Payne",
+          email: "jjrpayne@hotmail.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
@@ -18,8 +42,15 @@ app.use(router);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
 app.use("/", require("./routes/products"));
 
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,18 +1,146 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         -id
+ *         -productName
+ *         -productOwnerName
+ *         -Developers
+ *         -scrumMasterName
+ *         -startDate
+ *         -methodology
+ *         -location
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the product
+ *         productName:
+ *           type: string
+ *           description: The name of the product
+ *         productOwnerName:
+ *           type: string
+ *           description: The name of the product owner
+ *         Developers:
+ *           type: array of strings
+ *           description: A list of the developers who have worked on the product (minimum of 1, maximum of 5)
+ *         scrumMasterName:
+ *           type: string
+ *           description: The name of the product scrum master
+ *         startDate:
+ *           type: string
+ *           format: date
+ *           description: The start date of product development (format YYYY/MM/DD)
+ *         methodology:
+ *           type: string
+ *           description: The methodology of development (must be Agile or Waterfall)
+ *         location:
+ *           type: string
+ *           description: The URL of the git repository where the product is located
+ *           
+ *       example:
+ *         id: 1
+ *         productName: "Respect Economy"
+ *         productOwnerName: "Nick Mullen"
+ *         Developers: ["Stavros Halkias", "Adam Friedland"]
+ *         scrumMasterName: "Andre Steakhouse"
+ *         startDate: "2021/10/25"
+ *         methodology: "Waterfall"
+ *         location: "https://github.com/bcgov/BC-Policy-Framework-For-GitHub"
+ */
+
 const express = require("express");
 const router = express.Router();
 const ProductController = require('../controllers/products');
 const val = require("../tools/validate");
+
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Get a list of all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: A list of all products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: An error occured while retrieving products
+ *
+ */
 
 // get all products
 router.get('/api/products',
     ProductController.getAllProducts
 );
 
+/**
+* @swagger
+* /api/new_product:
+*   post:
+*     summary: Create a new product
+*     tags: [Products]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Product'
+*     responses:
+*       200:
+*         description: The created product.
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Product'
+*       500:
+*         description: An error occured while creating the new product
+*/
+
 // add new product
 router.post('/api/new_product',
     val.validateNewProduct(),
     ProductController.addNewProduct
 );
+
+/**
+* @swagger
+* /api/edit_product/{id}:
+*   put:
+*     summary: Edit an existing product by its id
+*     tags: [Products]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: integer
+*           required: true
+*         description: The product id
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Product'
+*     responses:
+*       200:
+*         description: The created product.
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Product'
+*       404:
+*         description: Could not find a product with the specified id
+*       500:
+*         description: An error occured while updating the product
+*/
 
 // edit product
 router.put('/api/edit_product/:id',
